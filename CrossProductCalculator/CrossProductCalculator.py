0a9dcb253ae6eac8a944ebdc18cea73f9eb8ed80
BlueShootingStar <BlueShootingStar1809@gmail.com>
2020-01-08 16:16:23 +0700

    First commit

--- CrossProductCalculator/CrossProductCalculator.py
+++ CrossProductCalculator/CrossProductCalculator.py
@@ -1,24 +1,47 @@
-while True:
-    vectorAx, vectorAy, vectorAz = input('Enter the x, y, z coordinates of your first vector: ').split()
-    try:
-        vectorAx = int(vectorAx)
-        vectorAy = int(vectorAy)
-        vectorAz = int(vectorAz)
-    except ValueError:
-        print('Insert integers for your coordinates.')
-        continue
+from model.Vector import *
+import sys
+
+def main():
     while True:
-        vectorBx, vectorBy, vectorBz = input('Enter the x, y, z coordinates of your second vector: ').split()
-        try:
-            vectorBx = int(vectorBx)
-            vectorBy = int(vectorBy)
-            vectorBz = int(vectorBz)
-        except ValueError:
-            print('Insert integers for your coordinates.')
-            continue
-        break
-    crossProductABx = vectorAy*vectorBz - vectorBy*vectorAz
-    crossProductABy = -(vectorAx*vectorBz - vectorBx*vectorAz)
-    crossProductABz = vectorAx*vectorBy - vectorBx*vectorAy
-    print('the cross product between the first and second vector is',crossProductABx,crossProductABy,crossProductABz)
-    break
+        vectorA = None
+        vectorB = None
+        vectorAB = None
+
+        while True:
+            try:
+                vectorAx, vectorAy, vectorAz = input('Enter the x, y, z coordinates of your first vector: ').split()
+                vectorA = Vector(int(vectorAx), int(vectorAy), int(vectorAz))
+                break
+            except:
+                print('Insert 3 integers spliting by a space character for your coordinates.')
+                continue
+
+        while True:
+            try:
+                vectorBx, vectorBy, vectorBz = input('Enter the x, y, z coordinates of your second vector: ').split()
+                vectorB = Vector(int(vectorBx), int(vectorBy), int(vectorBz))
+                break
+            except:
+                print('Insert 3 integers spliting by a space character for your coordinates.')
+                continue
+        
+        vectorAB = Vector.calculateCrossProduct(vectorA, vectorB)
+
+        print('The cross product between the first and second vector is:', vectorAB.toString())
+
+        yes = {'yes','y', 'ye', ''}
+        no = {'no','n'}
+
+        while True:
+            choice = input("*** Restart program? [Y/n]: ").lower()
+
+            if choice in yes:
+                break
+            elif choice in no:
+                sys.exit()
+            else:
+                print("Please respond with 'yes' or 'no'")
+
+        print()
+main()
+
